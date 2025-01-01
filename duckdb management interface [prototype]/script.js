8798198cd8a97e9061498c93e25d7f682ecebba0
ines <ineskadjii@gmail.com>
2025-01-01 21:01:43 +0100

    creation de la table task_congif et evaluation_results

--- duckdb management interface [prototype]/script.js
+++ duckdb management interface [prototype]/script.js
@@ -1,4 +1,15 @@
-console.log("script.js chargé");
+class CustomLogger {
+    log() {
+        // Pas d'action, logs désactivés
+    }
+    warn() {
+        // Pas d'action, warnings désactivés
+    }
+    error() {
+        // Affiche uniquement les erreurs
+        console.error(...arguments);
+    }
+}
 
 const getDb = async () => {
     try {
@@ -18,7 +29,7 @@ const getDb = async () => {
 
         // Instantiate the asynchronus version of DuckDB-wasm
         const worker = new Worker(worker_url);
-        const logger = new duckdb.ConsoleLogger();
+        const logger = new CustomLogger();
         const db = new duckdb.AsyncDuckDB(logger, worker);
         await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
         URL.revokeObjectURL(worker_url);
@@ -32,13 +43,19 @@ const getDb = async () => {
 };
 
 import * as duckdbduckdbWasm from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev106.0/+esm";
-console.log("duckdbduckdbWasm:", window.duckdbduckdbWasm);
 window.duckdbduckdbWasm = duckdbduckdbWasm;
 
 let temp_json_data;
 let temp_json_data2;
 let db;
 let conn;
+let configTasks;
+let tasksArray;
+let results;
+let results2;
+let resultArray;
+let resultArray2;
+
 
 async function loadJsonFiles() {
     try {
@@ -61,12 +78,76 @@ async function loadJsonFiles() {
             SELECT * FROM read_json_auto('http://localhost:10000/results2.json');
         `);
 
+        const filePath = 'http://localhost:10000/results1.json';
+        const filePath2 = 'http://localhost:10000/results2.json';
+
+        const response = await fetch(filePath);  // Attendre la réponse
+        const data = await response.json();      // Attendre la conversion en JSON
+
+        // Transformer 'config_tasks' en tableau d'objets
+        configTasks = data.config_tasks;
+        tasksArray = Object.keys(configTasks).map(key => {
+            const task = configTasks[key];
+            return {
+                task_base_name: task.name,
+                prompt_function: task.prompt_function,
+                hf_repo: task.hf_repo,
+                hf_subset: task.hf_subset,
+                trust_dataset: task.trust_dataset,
+                generation_size: task.generation_size,
+                original_num_docs: task.original_num_docs,
+                effective_num_docs: task.effective_num_docs,
+                must_remove_duplicate_docs: task.must_remove_duplicate_docs,
+                version: task.version,
+                frozen: task.frozen,
+            };
+        });
+
+        results = data.results;
+        resultArray = Object.keys(results).map(key => {
+            const result = results[key];
+            return {
+                key,
+                em: result.em,
+                em_stderr: result.em_stderr,
+                qem: result.qem,
+                qem_stderr: result.qem_stderr,
+                pem: result.pem,
+                pem_stderr: result.pem_stderr,
+                pqem: result.pqem,
+                pqem_stderr: result.pqem_stderr,
+            };
+        });
+
+        const response2 = await fetch(filePath2);  // Attendre la réponse
+        const data2 = await response2.json();
+
+        results2 = data2.results;
+        resultArray2 = Object.keys(results2).map(key => {
+            const result = results2[key];
+            return {
+                key,
+                em: result.em,
+                em_stderr: result.em_stderr,
+                qem: result.qem,
+                qem_stderr: result.qem_stderr,
+                pem: result.pem,
+                pem_stderr: result.pem_stderr,
+                pqem: result.pqem,
+                pqem_stderr: result.pqem_stderr,
+            };
+        });
+
+        // Afficher le tableau pour vérifier
+        //console.log(tasksArray);
+        //console.log(resultArray);
+
     } catch (error) {
         console.error("Erreur lors du chargement des fichiers JSON :", error);
     }
 }
 
-async function loadEvaluastionRuns(){
+async function loadEvaluationRuns(){
     try {
     // Table evaluation_runs
     await conn.query(`
@@ -84,7 +165,8 @@ async function loadEvaluastionRuns(){
                 total_evaluation_time VARCHAR
             );
             
-            INSERT INTO evaluation_runs (run_id, model_name, num_fewshot_seeds, override_batch_size, max_samples, job_id, start_time, end_time, total_evaluation_time)
+            INSERT INTO evaluation_runs (run_id, model_name, num_fewshot_seeds, override_batch_size, max_samples, job_id, start_time,
+             end_time, total_evaluation_time)
             SELECT 
               nextval('run_id_sequence'),
               config_general.model_name,
@@ -97,7 +179,8 @@ async function loadEvaluastionRuns(){
               config_general.total_evaluation_time_secondes
             FROM temp_json_data;
             
-            INSERT INTO evaluation_runs (run_id, model_name, num_fewshot_seeds, override_batch_size, max_samples, job_id, start_time, end_time, total_evaluation_time)
+            INSERT INTO evaluation_runs (run_id, model_name, num_fewshot_seeds, override_batch_size, max_samples, job_id, start_time,
+             end_time, total_evaluation_time)
             SELECT 
               nextval('run_id_sequence'),
               config_general.model_name,
@@ -114,66 +197,63 @@ async function loadEvaluastionRuns(){
     const result = await conn.query("SELECT * FROM evaluation_runs;");
     console.log("Données de evaluation_runs : ", result.toString());
 
-    conn.close();
 
     } catch (error) {
         console.error("Erreur lors du chargement des fichiers JSON :", error);
+        conn.close();
     }
 }
 
-/*async function loadTaskConfig() {
+async function loadTaskConfig() {
     try {
-        // Extraire les objets `config_task` du fichier JSON
-        // Vous pouvez supposer que `config_task` est une clé dans l'objet JSON global
-        const configTasks = await conn.query(`
-            SELECT *
-            FROM temp_json_data
-            WHERE json_extract(json_data, '$.config_tasks') IS NOT NULL
+        await conn.query(`
+
+            CREATE SEQUENCE task_id_sequence START 1;
+
+            CREATE TABLE IF NOT EXISTS task_configs (
+                task_id INTEGER PRIMARY KEY,
+                task_base_name VARCHAR NOT NULL,
+                prompt_function VARCHAR,
+                hf_repo VARCHAR,
+                hf_subset VARCHAR,
+                trust_dataset BOOLEAN,
+                generation_size INTEGER,
+                original_num_docs INTEGER,
+                effective_num_docs INTEGER,
+                must_remove_duplicate_docs BOOLEAN,
+                version INTEGER,
+                frozen BOOLEAN,
+            );
         `);
 
-        // Pour chaque objet `config_task`, extraire les sous-objets et insérer dans la base de données
-        configTasks.forEach(task => {
-            const configTaskData = JSON.parse(task.value).config_tasks;
+        // Insérer toutes les données dans une seule transaction
+        for (const task of tasksArray) {
+            await conn.query(`
+                INSERT INTO task_configs (task_id, task_base_name, prompt_function, hf_repo, hf_subset,
+                                          trust_dataset, generation_size,
+                                          original_num_docs, effective_num_docs, must_remove_duplicate_docs,
+                                          version, frozen)
+                VALUES (nextval('task_id_sequence'), '${task.task_base_name}',
+                        '${task.prompt_function}', '${task.hf_repo}',
+                        '${task.hf_subset}', '${task.trust_dataset}',
+                        '${task.generation_size}', '${task.original_num_docs}',
+                        '${task.effective_num_docs}', '${task.must_remove_duplicate_docs}', '${task.version}',
+                        '${task.frozen}');
+            `);
+        }
 
-            // Si config_tasks est un objet, vous devez insérer chaque sous-objet comme une ligne dans la table
-            Object.keys(configTaskData).forEach(taskKey => {
-                const configTask = configTaskData[taskKey];
+        // Vérifier l'insertion
+        const result = await conn.query("SELECT * FROM task_configs LIMIT 5;");
+        console.log("Données de task_configs : ", result.toString());
 
-                // Préparer l'insertion dans la table `task_configs`
-                const insertSQL = `
-                    INSERT INTO task_configs (task_id, task_base_name, prompt_function, hf_repo, hf_subset, hf_avail_splits, trust_dataset, evaluation_splits, generation_size, stop_sequence, original_num_docs, effective_num_docs, version, frozen, suite)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                `;
-
-                // Insérer les données spécifiques à chaque config_task
-                conn.run(insertSQL, [
-                    configTask.task_id,
-                    configTask.task_base_name,
-                    configTask.prompt_function,
-                    configTask.hf_repo,
-                    configTask.hf_subset,
-                    configTask.hf_avail_splits,
-                    configTask.trust_dataset,
-                    configTask.evaluation_splits,
-                    configTask.generation_size,
-                    configTask.stop_sequence,
-                    configTask.original_num_docs,
-                    configTask.effective_num_docs,
-                    configTask.version,
-                    configTask.frozen,
-                    configTask.suite
-                ]);
-            });
-        });
-
-        const result = await conn.query("SELECT * FROM task_configs;");
-        console.log("Données insérées dans task_configs:", result.toString());
-
-        conn.close();
     } catch (error) {
-        console.error("Erreur lors du chargement des fichiers JSON:", error);
+        // En cas d'erreur, annuler la transaction
+        console.error("Erreur lors du chargement des fichiers JSON :", error);
+        conn.close();
     }
-}*/
+}
+
+
 
 async function loadTaskMetrics(){
     try {
@@ -240,15 +320,76 @@ async function loadTaskMetrics(){
         const result = await conn.query("SELECT * FROM task_metrics;");
         console.log("Données de task_metrics : ", result.toString());
 
-        conn.close();
-
     } catch (error) {
         console.error("Erreur lors du chargement des fichiers JSON :", error);
+        conn.close();
     }
 }
 
+async function loadEvaluationResults(){
+    try {
+        await conn.query(`
+            
+            CREATE OR REPLACE TABLE evaluation_results (
+                run_id INTEGER REFERENCES evaluation_runs(run_id),
+                task_id INTEGER,
+                em DECIMAL(10,4), 
+                em_stderr DECIMAL(10,4),
+                qem DECIMAL(10,4), 
+                qem_stderr DECIMAL(10,4),
+                pem DECIMAL(10,4), 
+                pem_stderr DECIMAL(10,4), 
+                pqem DECIMAL(10,4),
+                pqem_stderr DECIMAL(10,4),
+                
+            );
+        `);
+
+        for (const result of resultArray) {
+            await conn.query(`
+                INSERT INTO evaluation_results (run_id, task_id, em, em_stderr, qem, qem_stderr, pem, pem_stderr, pqem, pqem_stderr)
+                VALUES (1,
+                        COALESCE(
+                        (SELECT task_id FROM task_configs WHERE '${result.key}' LIKE '%' || task_base_name || '%'),
+                        0
+                        ),
+                       ${result.em}, ${result.em_stderr},
+                       ${result.qem}, ${result.qem_stderr},
+                       ${result.pem}, ${result.pem_stderr},
+                       ${result.pqem}, ${result.pqem_stderr});
+            `);
+        }
+
+        for (const result of resultArray2) {
+            await conn.query(`
+                INSERT INTO evaluation_results (run_id, task_id, em, em_stderr, qem, qem_stderr, pem, pem_stderr, pqem, pqem_stderr)
+                VALUES (1,
+                        COALESCE(
+                        (SELECT task_id FROM task_configs WHERE '${result.key}' LIKE '%' || task_base_name || '%'),
+                        0
+                        ),
+                       ${result.em}, ${result.em_stderr},
+                       ${result.qem}, ${result.qem_stderr},
+                       ${result.pem}, ${result.pem_stderr},
+                       ${result.pqem}, ${result.pqem_stderr});
+            `);
+        }
+
+        // Vérifier l'insertion
+        const result = await conn.query("SELECT * FROM evaluation_results LIMIT 5");
+        console.log("Données de evaluation_results : ", result.toString());
+
+
+    } catch (error) {
+        console.error("Erreur lors du chargement des fichiers JSON :", error);
+        conn.close();
+    }
+}
+
+
 loadJsonFiles().then(() => {
-    loadEvaluastionRuns();
-    //loadTaskConfig(); -> Coucou
+    loadEvaluationRuns();
     loadTaskMetrics();
+    loadTaskConfig();
+    loadEvaluationResults();
 });
