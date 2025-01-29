d4a28d35666c5d6224509c8828148d78d31c3ab3
ariianel <lucariane03@gmail.com>
2025-01-30 00:11:27 +0100

    update code

--- duckdb management interface [prototype]/crud/crud-update.js
+++ duckdb management interface [prototype]/crud/crud-update.js
@@ -3,6 +3,249 @@ document.addEventListener("DOMContentLoaded", function () {
     const crudContenant = document.querySelector('.crud-contenant');
     const updateContenant = document.querySelector('.update-contenant');
     const updateBtn = document.getElementById('update-btn'); // Correction ici
+    const specificColumnsBtn = document.getElementById('specific-columns-in-row-btn');
+    
+    // U P D A T E : choose table to update 
+    const updateSimpleDisplayContenant = document.querySelector('.update-simple-display-contenant');
+    // U P D A T E : choose table to update >> 
+    const tableList = document.getElementById('table-list2');
+    const updateChooseColumnsContenant = document.querySelector('.update-choose-columns-contenant');
+    //table evaluation results
+    const evaluationResultsBtn = document.getElementById('update-evaluation-results-btn');
+    const evaluationResultsStep = document.getElementById('update-evaluation_results-table');
+    //table evaluation runs
+    const evaluationRunsBtn = document.getElementById('update-evaluation-runs-btn');
+    const evaluationRunsStep = document.getElementById('update-evaluation_runs-table');
+    //table task configs
+    const taskConfigsBtn = document.getElementById('update-task-configs-btn');
+    const taskConfigsStep = document.getElementById('update-task_configs-table');  
+    //table task metrics
+    const taskMetricsBtn = document.getElementById('update-task-metrics-btn');
+    const taskMetricsStep = document.getElementById('update-task_metrics-table'); 
+    //table task summaries
+    const taskSummariesBtn = document.getElementById('update-task-summaries-btn');
+    const taskSummariesStep = document.getElementById('update-tasks_summaries-table'); 
+    //table general summary
+    const generalSummaryBtn = document.getElementById('update-general-summary-btn');
+    const generalStep = document.getElementById('update-general_summary-table'); 
+
+    
+    // Données des colonnes des tables (simulées)
+    const tableColumns = {
+        "Table \"evaluation_runs\"": [
+            "run_id", "model_name", "num_fewshot_seeds", "override_batch_size", 
+            "max_samples", "job_id", "start_time", "end_time", "total_evaluation_time", 
+            "model_sha", "model_dtype", "model_size", "lighteval_sha"
+        ],
+        "Table \"task_configs\"": [
+            "task_id", "task_base_name", "prompt_function", "hf_repo", 
+            "hf_subset", "hf_revision", "hf_filter", "trust_dataset", 
+            "few_shots_split", "few_shots_select", "generation_size", 
+            "generation_grammar", "output_regex", "num_samples", 
+            "original_num_docs", "effective_num_docs", 
+            "must_remove_duplicate_docs", "version", "frozen"
+        ],
+        "Table \"task_metrics\"": [
+            "metric_id", "metric_name", "higher_is_better", "category", 
+            "use_case", "sample_level_fn", "corpus_level_fn"
+        ],
+        "Table \"tasks_evaluation_results\"": [
+            "run_id", "task_id", "em", "em_stderr", "qem", 
+            "qem_stderr", "pem", "pem_stderr", "pqem", "pqem_stderr"
+        ],
+        "Table \"aggregated_evaluation_results\"": [
+            "run_id", "result_type", "em", "em_stderr", "qem", 
+            "qem_stderr", "pem", "pem_stderr", "pqem", "pqem_stderr"
+        ],
+        "Table \"tasks_summaries\"": [
+            "run_id", "task_id", "truncated", "non_truncated", 
+            "padded", "non_padded", "effective_few_shots", 
+            "num_truncated_few_shots"
+        ],
+        "Table \"general_summary\"": [
+            "run_id", "truncated", "non_truncated", 
+            "padded", "non_padded", "num_truncated_few_shots"
+        ]
+    };
+
+    // U P D A T E >> simple display >> how to display data -----------------------------------------
+    
+    specificColumnsBtn.addEventListener("click", function () {
+        // Cacher le conteneur READ
+        updateContenant.style.display = 'none';
+        // Afficher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'flex';
+
+        // Afficher et masquer les étapes correspondantes
+        showStep('update-choose-table-step');
+    });
+
+    // Gestion de la fermeture de l'étape "simple display" !!!!!!!!!!!!!!!!!!!!!!!!!
+    const closeUpdateChooseStepBtn = document.querySelector(".update-choose-table-close-step");
+    if (closeUpdateChooseStepBtn) {
+        closeUpdateChooseStepBtn.addEventListener("click", function () {
+            // Cacher la section choose table et afficher update
+            updateContenant.style.display = "flex";
+            updateSimpleDisplayContenant.style.display = "none";
+            // Masquer l'étape read
+            hideStep('update-choose-table-step');
+            //masquer la table selectionnés
+            tableList.style.display = "none";
+            //masquer le conteneur "how to display data ?"
+            updateChooseColumnsContenant.style.display = "none";
+            // masquer les colonnes de la table
+            //columnsContainer.style.display = "none";
+
+        });
+
+
+    }
+
+
+    //gestion des tables ---
+
+    //EVALUATION RESULTS
+    evaluationResultsBtn.addEventListener("click", function () {
+       
+
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== evaluationResultsStep) {
+                item.style.display = "none";
+            }
+        });
+
+        // Afficher la table sélectionnée
+        evaluationResultsStep.style.display = 'flex';
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+        buffer = "Table \"evaluation_results\"";
+    });
+
+    //EVALUATION RUNS
+    evaluationRunsBtn.addEventListener("click", function () {
+
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== evaluationRunsStep) {
+                item.style.display = "none";
+            }
+        });
+
+        // Afficher la table sélectionnée
+        evaluationRunsStep .style.display = 'flex';
+        // Afficher le conteneur READ >> simple display >> how to display data
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+
+        buffer = "Table \"evaluation_runs\"";
+    });
+
+    //TASK CONFIGS
+    taskConfigsBtn.addEventListener("click", function () {
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== taskConfigsStep) {
+                item.style.display = "none";
+            }
+        });
+
+        // Afficher la table sélectionnée
+        taskConfigsStep.style.display = 'flex';
+        // Afficher le conteneur READ >> simple display >> how to display data
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+        buffer = "Table \"task_configs\"";
+    });
+
+    //TASK METRICS
+    taskMetricsBtn.addEventListener("click", function () {
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== taskMetricsStep) {
+                item.style.display = "none";
+            }
+        }); 
+
+        // Afficher la table sélectionnée
+        taskMetricsStep.style.display = 'flex';
+        // Afficher le conteneur READ >> simple display >> how to display data
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+        buffer = "Table \"task_metrics\"";
+
+    }); 
+    //TASK SUMMARIES
+    taskSummariesBtn.addEventListener("click", function () {
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== taskSummariesStep) {
+                item.style.display = "none";
+            }
+        }); 
+
+        // Afficher la table sélectionnée
+        taskSummariesStep.style.display = 'flex';
+        // Afficher le conteneur READ >> simple display >> how to display data
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+        buffer = "Table \"tasks_summaries\"";
+
+    }); 
+
+    ////GENERAL SUMMARY
+    generalSummaryBtn.addEventListener("click", function () {
+        // Afficher la liste des tables si elle est cachée
+        if (tableList.style.display === "none") {
+            tableList.style.display = "block"; // Affiche la liste complète des tables
+        }
+
+        // Cacher toutes les autres tables sauf celle sélectionnée
+        document.querySelectorAll(".table-item2").forEach(item => {
+            if (item !== generalStep) {
+                item.style.display = "none";
+            }
+        }); 
+
+        // Afficher la table sélectionnée
+        generalStep.style.display = 'flex';
+        // Afficher le conteneur READ >> simple display >> how to display data
+        updateChooseColumnsContenant.style.display = 'flex';
+        // Cacher le conteneur READ >> simple display
+        updateSimpleDisplayContenant.style.display = 'none';
+        buffer = "Table \"general_summary\"";
+
+    }); 
 
     if (updateBtn && crudContenant && updateContenant) {
         // Événement au clic du bouton UPDATE
